b00f9b187c45ada516599c3b080ad77fd5e1bef3
Danielteles999 <107625244+Danielteles999@users.noreply.github.com>
2022-08-28 21:51:19 -0300

    Add files via upload

---
+++ codigo/Lab1 atv1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp69
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            for (int i = 1000; i <= 9999; i++)
+            {
+                string numInt = Convert.ToString(i);
+                char[] numero  = numInt.ToCharArray();
+
+                string Parte1 = Convert.ToString(numero[0]+numero[1]);
+                string Parte2 = Convert.ToString(numero[2]+numero[3]);
+
+                Console.WriteLine(Parte1);
+
+                int Parte1conv = int.Parse(Parte1);
+                int Parte2conv = int.Parse(Parte2);
+
+                int somatotal = Parte1conv + Parte2conv;
+
+                if (somatotal == i*i)
+                {
+                    Console.WriteLine(i);
+                }
+                
+            }
+        }
+    }
+}
---
+++ codigo/Lab1 atv2.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int cont = 0;
+
+            Console.WriteLine("Verificador de números primos");
+            Console.WriteLine(" ");
+            Console.Write("Insira o número a ser verificado: ");
+            int num = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= num; i++)
+            {
+                if (num%i == 0)
+                {
+                    cont++;
+                }
+            }
+
+            if (cont <= 2)
+            {
+                Console.WriteLine("O número {0} é primo!", num);
+                Console.ReadKey();
+            }
+            else
+            {
+                Console.WriteLine("O número {0} NÃO é primo!", num);
+                Console.ReadKey();
+            }
+
+
+            //Parte2
+            int soma = 0;
+
+            for (int j = 1; j <= 1000; j++)
+            {
+                for (int k = 1; k <= j; k++)
+                {
+
+                    int[] div = new int[k];
+
+                    if (j%k==0)
+                    {
+                       div[k] = k;
+                    }
+
+                    soma += div[k];
+
+                    if (soma==6)
+                    {
+                        Console.WriteLine(j);
+                    }
+
+                }
+            }
+        }
+
+    }
+}
