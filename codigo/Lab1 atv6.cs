eff72614bb87c15271c06b7ceb579bc414d40b7c
Daniel Teles <107625244+Danielteles999@users.noreply.github.com>
2022-10-12 21:02:40 -0300

    Update Lab1 atv6.cs

--- codigo/Lab1 atv6.cs
+++ codigo/Lab1 atv6.cs
@@ -6,34 +6,39 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleApp1;
-
-class Program
+namespace ConsoleApp1
 {
-    static void Main (string[] args)
+    class Program
     {
-        int num = 0, expoente = 0, pot=1, i = 0;
-
-
-        Console.WriteLine("Potenciação de números 5.3");
-        Console.Write("Insira o valor da base: ");
-        num = int.Parse(Console.ReadLine());
-        Console.Write("Insira o valor do expoente: ");
-        expoente = int.Parse(Console.ReadLine());
-
-
-        while (i<expoente)
+        static int Operador(ref int num, ref int expoente)
         {
-            pot = pot * num;
-            i++;
+            int pot = 1, i = 0;
 
-            if (expoente==0)
+            while (i < expoente)
             {
-                Console.WriteLine("A resposta é 1");
+                pot = pot * num;
+                i++;
             }
+
+            return pot;
         }
 
-        Console.WriteLine($"{num} elevado à {expoente} é: {pot}");
+        static void Main(string[] args)
+        {
+            int num = 0, expoente = 0;
 
+
+            Console.WriteLine("Potenciação de números 5.3");
+
+            Console.Write("Insira o valor da base: ");
+            num = int.Parse(Console.ReadLine());
+
+            Console.Write("Insira o valor do expoente: ");
+            expoente = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"{num} elevado à {expoente} é {Operador(ref num, ref expoente)}");
+            Console.ReadKey();
+
+        }
     }
 }
