ac694a6d0f396eebeed56d7e24417c56e57ac3d0
Daniel Teles <107625244+Danielteles999@users.noreply.github.com>
2022-10-16 14:36:41 -0300

    Update Lab2 atv5.cs

--- codigo/Lab2 atv5.cs
+++ codigo/Lab2 atv5.cs
@@ -4,45 +4,55 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleApp77
+namespace ConsoleApp91
 {
     class Program
     {
-
-        static int ocorrencias(char[] fraseChar, char letra)
-        {
-            int i = 0;
-
-            while (i != frase.Length)
-            {
-                if (frase[i] == letra)
-                {
-                    return 1 + ocorrencias(frase[i + 1], letra);
-                }
-
-                i++;
-                return 0;
-            }
-        }
-
         static void Main(string[] args)
         {
-            string frase = "";
-            char letra = 'a';
+            Console.WriteLine("Contabilizador de caracteres v3.6 \n");
+            Console.WriteLine("Insira a frase desejada: ");
+            string frase = Console.ReadLine();
+            Console.WriteLine("Insira a letra para a busca: ");
+            char letra = char.Parse(Console.ReadLine());
 
-            char[] = new char[]
+            char[] fraseChar = frase.ToCharArray();//Convertendo...
 
-            Console.WriteLine("Frequencia de caracteres em uma frase Recursivo");
-            Console.Write("Insira a frase desejada: ");
-            frase = Convert.ToString(Console.ReadLine());
+            Console.WriteLine($"A letra {letra} aparece {contab(fraseChar, letra, 0)} vezes na frase");
+            Console.ReadKey();
+        }
 
-            Console.Write("Insira a letra a ser contada na frase: ");
-            letra = Convert.ToChar(Console.ReadLine());
+        static int contab(char[] fraseChar, char letra, int num)
+        {
+            int soma = 0;
 
-            char[] fraseChar = frase.ToCharArray();
-
-            ocorrencias(fraseChar[], letra);
+            if (num == fraseChar.Length-1)
+            {
+                if (fraseChar[num]==letra)
+                {
+                    return 1;
+                }
+                else
+                {
+                    return 0;
+                }
+            }
+            else
+            {
+                if (fraseChar[num] == letra)
+                {
+                    soma = 1 + contab(fraseChar, letra, num + 1);
+                    return soma;
+                }
+                else
+                {
+                    soma = 0 + contab(fraseChar, letra, num + 1);
+                    return soma;
+                }
+            }
 
         }
+
+
     }
 }
